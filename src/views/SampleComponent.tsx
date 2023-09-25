0bb195f9bbf15a504171144fa4a4b58dcd84d401
rofiq <abdul.hidayat@asyx.com>
2023-09-25 17:35:10 +0700

    Export import component

---
+++ src/views/SampleComponent.tsx
@@ -0,0 +1,55 @@
+import React, { Component } from 'react';
+import {
+  Image,
+  SafeAreaView,
+  ScrollView,
+  Text,
+  TextInput,
+  View,
+} from 'react-native';
+
+function SampleComponent(): JSX.Element {
+  return (
+    <View>
+      <View style={{ width: 80, height: 80, backgroundColor: '#00d2d3' }} ></View>
+      <View>
+        <Text>Ini Komponen Home</Text>
+      </View>
+      <Images />
+      <TextInput style={{ borderWidth: 1 }} ></TextInput>
+      <BoxGreen />
+      <Profile />
+    </View>
+  );
+}
+
+function Images(): JSX.Element {
+  return (
+    <Image 
+      source={{ uri: 'https://picsum.photos/100' }} 
+      style={{ width: 100, height: 100 }}
+    />
+  );
+}
+
+class BoxGreen extends Component {
+  render() {
+    return <Text>Ini class komponen</Text>
+  }
+}
+
+class Profile extends Component {
+  render() {
+    return (
+      <View>
+        <Image
+          source={{ uri: 'https://picsum.photos/100' }} 
+          style={{ width: 100, height: 100, borderRadius: 50 }}
+        />
+        <Text style={{ color: 'blue', fontSize: 24 }} >Ini profile</Text>
+      </View>
+    )
+  }
+}
+
+export default SampleComponent;
---
+++ src/views/StylingReactNativeComponent.tsx
@@ -0,0 +1,89 @@
+import {
+  Image,
+  StyleSheet,
+  Text,
+  View,
+} from 'react-native';
+import macbook from '../assets/images/macbook.jpg'
+
+function StylingReactNativeCompoonent(): JSX.Element {
+  return (
+    <View>
+      <Text style={styles.text} >Styling Component</Text>
+      <View style={{ 
+          width: 100,
+          height: 100,
+          backgroundColor: '#00d2d3',
+          borderWidth: 2,
+          borderColor: '#01a3a4',
+          marginTop: 20,
+          marginLeft: 20,
+        }} 
+      />
+      <View style={{ 
+          padding: 12,
+          backgroundColor: '#f2f2f2',
+          width: 212,
+          borderRadius: 8
+        }}
+      >
+        <Image source={macbook} 
+          style={{
+            width: 188,
+            height: 107,
+            borderRadius: 4,
+          }} 
+        />
+        <Text style={{
+            fontSize: 14,
+            fontWeight: 'bold',
+            marginTop: 16,
+          }}
+        >New Macbook Pro 2019
+        </Text>
+        <Text style={{
+            fontSize: 12,
+            fontWeight: 'bold',
+            marginTop: 12,
+            color: '#f2994a',
+          }}
+        >Rp. 25.000.000
+        </Text>
+        <Text style={{
+            fontSize: 12,
+            fontWeight: '300',
+            marginTop: 12,
+          }}
+        >Jakarta Barat
+        </Text>
+        <View style={{
+            backgroundColor: '#6fcf97',
+            marginTop: 20,
+            paddingVertical: 6,
+            borderRadius: 16,
+          }}
+        >
+          <Text style={{
+              fontSize: 14,
+              fontWeight: '600',
+              color: 'white',
+              textAlign: 'center',
+            }}
+          >BELI</Text>
+        </View>
+      </View>
+    </View>
+  )
+}
+
+const styles = StyleSheet.create({
+  text: {
+    fontSize: 18,
+    fontWeight: 'bold',
+    color: '#222f3e',
+    marginLeft: 20,
+    marginTop: 40,
+  }
+});
+
+export default StylingReactNativeCompoonent;
